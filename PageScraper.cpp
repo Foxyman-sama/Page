d73b0c4d5fb936398d96383f44b30fe00d43d70e
Foxymansama <dimalatoho1448@gmail.com>
2022-11-21 17:50:09 +0200

    Start PageScraper project

---
+++ PageScraper.cpp
@@ -0,0 +1,12 @@
+﻿// PageScraper.cpp: определяет точку входа для приложения.
+//
+
+#include "PageScraper.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
